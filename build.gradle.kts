83df2f0d914f1b756c4022f4a741d6229d8043e6
__aduii__ <alexjuep@hotmail.com>
2023-09-03 20:28:19 -0500

    Add manifest gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,4 +36,7 @@ tasks.withType<Test> {
 }
 tasks.jar {
     archiveFileName.set("marketv2-1.0.jar")
+    manifest {
+        attributes["Main-Class"] = "com.platzi.marketv2.Marketv2Application"
+    }
 }
